2f814085d8528874caeba4e697f243c42029705c
ramonier-ramos <ramonzramos@hotmail.com>
2016-04-22 19:10:20 -0400

    Added files via upload

---
+++ C#, .net/Account/Account/Account.cs
@@ -0,0 +1,48 @@
+﻿// Programming Problem 4_2
+// Filename: Account.cs
+// Account class with a Credit method that deposits money into the account
+// and a Debit method that withdraws money from the account.
+using System;
+
+public class Account
+{
+    // Declare instance variable to store account balance
+    private decimal balance;
+
+    // Property to get and set balance
+    public decimal Balance
+    {
+        get
+        {
+            return balance;
+        } // End get
+        set
+        {
+            // Validate that value is greater than or equal to 0; 
+            // if it is not, balance is left unchanged
+            if (value >= 0)
+                balance = value;
+        } // End set
+    } // End property Balance
+
+    // Constructor  
+    public Account(decimal initialBalance)
+    {
+        Balance = initialBalance; // Set balance using property
+    } // End constructor Account
+
+    // Credits (adds) an amount to the account balance
+    public void Credit(decimal amount)
+    {
+        Balance = Balance + amount; // Add amount to balance 
+    } // End method Credit
+
+    // Debits (subtracts) an amount from the account balance
+    public void Debit(decimal amount)
+    {
+        if (amount > Balance)
+            Console.WriteLine("Debit amount exceeded account balance.\n");
+
+        Balance = Balance - amount; // Subtract amount from balance 
+    } // End method Debit
+} // End class Account
---
+++ C#, .net/Account/Account/AccountTest.cs
@@ -0,0 +1,70 @@
+﻿// Programming Problem 4_2
+// Filename: AccountTest.cs
+// Demonstrates Account class's capabilities.
+using System;
+
+public class AccountTest
+{
+    // Main method begins program execution
+    public static void Main(string[] args)
+    {
+        // Declare variables to hold amounts entered by user
+        decimal depositAmount;
+        decimal withdrawalAmount;
+
+        // Call Account class to create instances of that class (objects)
+        Account account1 = new Account(50.00M); // Create first Account object
+        Account account2 = new Account(-7.53M); // create second Account object
+
+        // Display initial balance of each object
+        Console.WriteLine("account1 balance: {0:C}", account1.Balance);
+        Console.WriteLine("account2 balance: {0:C}\n", account2.Balance);
+
+        // First, test the Credit method of the Account class
+
+        // Prompt and obtain user input
+        Console.Write("Enter deposit amount for account1: ");
+        depositAmount = Convert.ToDecimal(Console.ReadLine());
+        Console.WriteLine("\nAdding {0:C} to account1 balance\n", depositAmount);
+        account1.Credit(depositAmount); // Add to account1 balance
+
+        // Display account balances
+        Console.WriteLine("account1 balance: {0:C}", account1.Balance);
+        Console.WriteLine("account2 balance: {0:C}\n", account2.Balance);
+
+        // Prompt and obtain user input
+        Console.Write("Enter deposit amount for account2: ");
+        depositAmount = Convert.ToDecimal(Console.ReadLine());
+        Console.WriteLine("\nAdding {0:C} to account2 balance\n", depositAmount);
+        account2.Credit(depositAmount); // Add to account2 balance
+
+        // Display account balances
+        Console.WriteLine("account1 balance: {0:C}", account1.Balance);
+        Console.WriteLine("account2 balance: {0:C}\n", account2.Balance);
+
+        // Second, test the Debit method of the Account class
+
+        // Prompt and obtain user input
+        Console.Write("Enter withdrawal amount for account1: ");
+        withdrawalAmount = Convert.ToDecimal(Console.ReadLine());
+        Console.WriteLine("\nSubtracting {0:C} from account1 balance\n",
+           withdrawalAmount);
+        account1.Debit(withdrawalAmount); // Subtract amount from account1
+
+        // Display account balances
+        Console.WriteLine("account1 balance: {0:C}", account1.Balance);
+        Console.WriteLine("account2 balance: {0:C}\n", account2.Balance);
+
+        // Prompt and obtain user input
+        Console.Write("Enter withdrawal amount for account2: ");
+        withdrawalAmount = Convert.ToDecimal(Console.ReadLine());
+        Console.WriteLine("\nSubtracting {0:C} from account2 balance\n",
+           withdrawalAmount);
+        account2.Debit(withdrawalAmount); // Subtract amount from account2
+
+        // Display account balances
+        Console.WriteLine("account1 balance: {0:C}", account1.Balance);
+        Console.WriteLine("account2 balance: {0:C}", account2.Balance);
+    } // End method Main
+} // End class AccountTest
+

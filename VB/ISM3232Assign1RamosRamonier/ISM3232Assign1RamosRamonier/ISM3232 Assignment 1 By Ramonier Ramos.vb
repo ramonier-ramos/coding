2481152c97944cbdda96f0b5a8b9c62217f06e06
ramonier-ramos <ramonzramos@hotmail.com>
2016-04-22 20:11:54 -0400

    Added files via upload

---
+++ VB/ISM3232Assign1RamosRamonier/ISM3232Assign1RamosRamonier/ISM3232 Assignment 1 By Ramonier Ramos.vb
@@ -0,0 +1,10 @@
+﻿Public Class frmISM3232Assignment1ByRamonierRamos
+
+    Private Sub btnClickMe_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClickMe.Click
+        Dim YourResponse As String
+        YourResponse = InputBox("Please type your name here and click ok")
+        MsgBox("Hello " & YourResponse)
+        MsgBox("Thank you for visiting my assignment1 in ISM3232")
+        MsgBox("This is going to be a fun class in which I will learn a lot about programming a computer!")
+    End Sub
+End Class
---
+++ VB/ISM3232Assign2RamosRamonier/frmISM3232AssignmentByRamonierRamos.vb
@@ -0,0 +1,45 @@
+﻿Public Class frmISM3232AssignmentByRamonierRamos
+
+    Private Sub btnDisplayAge_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDisplayAge.Click
+        Const YearOfBirth As Single = 2014
+        Dim Age As Single
+
+        Age = YearOfBirth - txtYearofBirth.Text
+
+
+        MsgBox("Dear " & txtFirstName.Text & ", you are approximately " & Age & " years old ")
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub btnDisplaySalesTax_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDisplaySalesTax.Click
+
+
+        Const SalesTaxRate As Single = 0.07
+        Dim SalesTax As Single
+
+        SalesTax = SalesTaxRate * txtSales.Text
+
+        MsgBox("Your sales tax is: " & Format(SalesTax, "Currency"))
+
+
+
+
+
+    End Sub
+End Class
